90e19c3768ed0296b8536a0b69fe0321cd810ae1
ilhamroliz <ilham.rolis@gmail.com>
2018-11-27 14:04:36 +0700

    roliz

--- app/Http/Controllers/PlasmafoneController.php
+++ app/Http/Controllers/PlasmafoneController.php
@@ -12,7 +12,6 @@ class PlasmafoneController extends Controller
     public static function getActivity()
     {
         Carbon::setLocale('id');
-        $sekarang = Carbon::now('Asia/Jakarta');
         $lastLogin = DB::table('d_mem')
             ->where('m_id', '=', Auth::user()->m_id)
             ->select('m_lastlogin')
@@ -25,5 +24,34 @@ class PlasmafoneController extends Controller
         }
         $time = $lastLogin->diffForHumans();
         return $time;
+    }
+
+    public static function getHari(){
+        $hari = Carbon::now('Asia/Jakarta')->format('l');
+
+        if ($hari == 'Sunday') {
+            $hari = 'Ahad';
+        } elseif ($hari == 'Monday'){
+            $hari = 'Senin';
+        } elseif ($hari == 'Tuesday'){
+            $hari = 'Selasa';
+        } elseif ($hari == 'Wednesday'){
+            $hari = 'Rabu';
+        }
+        elseif ($hari == 'Thursday'){
+            $hari = 'Kamis';
+        }
+        elseif ($hari == 'Friday'){
+            $hari = 'Jumat';
+        }
+        elseif ($hari == 'Saturday'){
+            $hari = 'Sabtu';
+        }
+        return $hari;
+    }
+
+    public static function getTanggal(){
+        $date = Carbon::now('Asia/Jakarta')->format('d M Y');
+        return $date;
     }
 }
--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -7,11 +7,11 @@
 		<div class="col-xs-6 col-sm-6 text-right hidden-xs">
 			<div class="txt-color-white inline-block">
 				<i class="txt-color-blueLight hidden-mobile">Last account activity <i class="fa fa-clock-o"></i> <strong>{{-- 52 mins ago &nbsp; --}}{{ App\Http\Controllers\PlasmafoneController::getActivity() }}</strong> </i>
-				<div class="btn-group dropup">
+				{{-- <div class="btn-group dropup">
 					<button class="btn btn-xs dropdown-toggle bg-color-blue txt-color-white" data-toggle="dropdown">
 						<i class="fa fa-link"></i> <span class="caret"></span>
 					</button>
-				</div>
+				</div> --}}
 			</div>
 		</div>
 	</div>
