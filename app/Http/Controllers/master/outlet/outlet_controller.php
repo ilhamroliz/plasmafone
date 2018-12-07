1cef8462ded5c663b22e1a130f359b7a70ed8088
Syahrul Yusuf <syahrulyusuf97@gmail.com>
2018-12-07 17:16:21 +0700

    fix outlet

--- app/Http/Controllers/master/outlet/outlet_controller.php
+++ app/Http/Controllers/master/outlet/outlet_controller.php
@@ -19,17 +19,61 @@ class outlet_controller extends Controller
     	return view('master.outlet.index');
     }
 
-    public function getdata()
+    public function detail($id)
     {
-        $dataOutlet = Outlet::orderBy('c_id', 'desc')->get();
+        $outlet = Outlet::where(['c_id' => $id])->first();
+        return response()->json($outlet);
+    }
 
-        $dataOutlet = collect($dataOutlet);
+    public function getdataactive()
+    {
+        $outlet_active = Outlet::where('c_isactive', 'Y')->orderBy('created_at', 'desc')->get();
 
-        return DataTables::of($dataOutlet)
+        $outlet_active = collect($outlet_active);
 
-        ->addColumn('aksi', function ($dataOutlet){ 
+        return DataTables::of($outlet_active)
 
-            return '<div class="text-center"><button class="btn btn-xs btn-primary btn-circle view" data-toggle="tooltip" data-placement="top" title="Lihat Data" onclick="detail(\'' . $dataOutlet->c_id . '\')"><i class="glyphicon glyphicon-list-alt"></i></button>&nbsp;<button class="btn btn-xs btn-warning btn-circle" data-toggle="tooltip" data-placement="top" title="Edit Data" onclick="edit(\'' . $dataOutlet->c_id . '\')"><i class="glyphicon glyphicon-edit"></i></button></div>';
+        ->addColumn('aksi', function ($outlet_active){ 
+
+            return '<div class="text-center"><button class="btn btn-xs btn-primary btn-circle view" data-toggle="tooltip" data-placement="top" title="Lihat Data" onclick="detail(\'' . $outlet_active->c_id . '\')"><i class="glyphicon glyphicon-list-alt"></i></button>&nbsp;<button class="btn btn-xs btn-warning btn-circle" data-toggle="tooltip" data-placement="top" title="Edit Data" onclick="edit(\'' . $outlet_active->c_id . '\')"><i class="glyphicon glyphicon-edit"></i></button>&nbsp;<button class="btn btn-xs btn-danger btn-circle" data-toggle="tooltip" data-placement="top" title="Hapus Data" onclick="hapus(\'' . $outlet_active->c_id . '\')"><i class="glyphicon glyphicon-trash"></i></button></div>';
+
+        })
+
+        ->rawColumns(['aksi'])
+
+        ->make(true);
+    }
+
+    public function getdataall()
+    {
+        $outlet_all = Outlet::orderBy('created_at', 'desc')->get();
+
+        $outlet_all = collect($outlet_all);
+
+        return DataTables::of($outlet_all)
+
+        ->addColumn('aksi', function ($supplier_all){    
+
+            return '<div class="text-center"><button class="btn btn-xs btn-primary btn-circle view" data-toggle="tooltip" data-placement="top" title="Lihat Data" onclick="detail(\'' . $supplier_all->c_id . '\')"><i class="glyphicon glyphicon-list-alt"></i></button>&nbsp;<button class="btn btn-xs btn-warning btn-circle" data-toggle="tooltip" data-placement="top" title="Edit Data" onclick="edit(\'' . $supplier_all->c_id . '\')"><i class="glyphicon glyphicon-edit"></i></button>&nbsp;<button class="btn btn-xs btn-danger btn-circle" data-toggle="tooltip" data-placement="top" title="Hapus Data" onclick="hapus(\'' . $supplier_all->c_id . '\')"><i class="glyphicon glyphicon-trash"></i></button></div>';
+
+        })
+
+        ->rawColumns(['aksi'])
+
+        ->make(true);
+    }
+
+    public function getdatanonactive()
+    {
+        $outlet_nonactive = Outlet::where('c_isactive', 'N')->orderBy('created_at', 'desc')->get();
+
+        $outlet_nonactive = collect($outlet_nonactive);
+
+        return DataTables::of($outlet_nonactive)
+
+        ->addColumn('aksi', function ($outlet_nonactive){     
+
+            return '<div class="text-center"><button class="btn btn-xs btn-primary btn-circle view" data-toggle="tooltip" data-placement="top" title="Lihat Data" onclick="detail(\'' . $outlet_nonactive->c_id . '\')"><i class="glyphicon glyphicon-list-alt"></i></button>&nbsp;<button class="btn btn-xs btn-warning btn-circle" data-toggle="tooltip" data-placement="top" title="Edit Data" onclick="edit(\'' . $outlet_nonactive->c_id . '\')"><i class="glyphicon glyphicon-edit"></i></button>&nbsp;<button class="btn btn-xs btn-danger btn-circle" data-toggle="tooltip" data-placement="top" title="Hapus Data" onclick="hapus(\'' . $outlet_nonactive->c_id . '\')"><i class="glyphicon glyphicon-trash"></i></button></div>';
 
         })
 
@@ -120,4 +164,154 @@ class outlet_controller extends Controller
 
     	return view('master.outlet.add');
     }
+
+    public function edit(Request $request, $id = null)
+    {
+        if ($request->isMethod('post')) {
+
+            $data       = $request->all();
+
+            DB::beginTransaction();
+
+            try {
+
+                $check_code = Outlet::where('c_id', $data['code'])->count();
+                $check_name = Outlet::where('c_name', '=', $data['name'])->where('c_id', '!=', $data['code'])->count();
+
+                if ($check_code == 0) {
+                    
+                    return  json_encode([
+                        'status'    => 'tidak ada',
+                        'code'       => $data['code']
+                    ]);
+
+                } else if ($check_name > 0){
+
+                    return  json_encode([
+                        'status'    => 'nama ada',
+                        'name'       => $data['name']
+                    ]);
+
+                } else {
+
+                    if ($data['note'] == "") {
+
+                        $note = "";
+
+                    } else {
+
+                        $note = strtoupper($data['note']);
+
+                    }
+
+                    Outlet::where(['c_id' => $data['code']])->update([
+                        'c_name'     => strtoupper($data['name']),
+                        'c_tlp'        => strtoupper($data['telp']),
+                        'c_address'     => strtoupper($data['address']),
+                        'c_note'        => $note,
+                        'c_isactive'    => strtoupper($data['isactive'])
+                    ]);
+
+                     DB::commit();
+
+                    // all good
+                    return  json_encode([
+                            'status'    => 'berhasil',
+                            'code'      => $data['code']
+                        ]);
+
+                }
+
+            } catch (\Exception $e) {
+
+                DB::rollback();
+
+                // something went wrong
+                return  json_encode([
+                            'status'    => 'gagal',
+                            'msg'       => $e
+                        ]);
+
+            }
+        }   
+
+        // ======================Method Get================================
+        DB::beginTransaction();
+
+        try {
+
+            $check = Outlet::where('c_id', $id)->count();
+
+            if ($check > 0) {
+
+                $outlets = Outlet::where('c_id', $id)->get();
+
+                DB::commit();
+                
+                return view('master.outlet.edit')->with(compact('outlets'));
+
+            } else {
+
+                return redirect()->back()->with('flash_message_error', 'Data yang anda edit tidak ada didalam basis data...! Mulai ulang halaman');
+
+            }
+
+        } catch (\Exception $e) {
+
+            DB::rollback();
+
+            // something went wrong
+            return redirect()->back()->with('flash_message_error', 'Ada yang tidak beres...! Mohon coba lagi');
+
+        }
+        
+    }
+
+    public function delete($id = null)
+    {
+        DB::beginTransaction();
+
+        try {
+
+            $check = Outlet::where('c_id', $id)->count();
+            $check_d_mem = DB::table('d_mem')->where(['m_comp' => $id])->count();
+
+            if ($check == 0) {
+                
+                return  json_encode([
+                    'status'    => 'tidak ada'
+                ]);
+
+            } else if ($check_d_mem > 0){
+
+                return  json_encode([
+                    'status'    => 'd_mem ada'
+                ]);
+
+            } else {
+
+                Outlet::where(['c_id' => $id])->delete();
+
+                DB::commit();
+
+                // all good
+                return  json_encode([
+                    'status'    => 'berhasil'
+                ]);
+
+            }
+
+        } catch (\Exception $e) {
+
+            DB::rollback();
+            
+            // something went wrong
+            return  json_encode([
+                'status'    => 'gagal',
+                'msg'       => $e
+            ]);
+
+        }
+    }
+
 }
