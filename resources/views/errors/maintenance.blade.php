530a46283012216f08897269c4ab0e18305c5cca
ilhamroliz <ilham.rolis@gmail.com>
2018-12-11 10:38:18 +0700

    roliz

---
+++ resources/views/errors/maintenance.blade.php
@@ -0,0 +1,71 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Perwita | Maintenance</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('assets/img/dboard/logo/sublogo2.png') }}"/>
+    <link href="{{ asset('assets/vendors/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('assets/css/inspiniaAnimate.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/inspiniaStyle.css') }}" rel="stylesheet">
+    <style type="text/css">
+           
+    .svg-computer {
+      stroke-dasharray: 1134;
+      stroke-dashoffset: -1134;
+      animation: draw-me 5s infinite;
+      animation-direction: normal;
+      height: 200px; }
+
+    @keyframes draw-me {
+      from {
+        stroke-dashoffset: -1134; }
+      to {
+        stroke-dashoffset: 0; } }
+    .svg-rocket {
+      height: 200px; }
+
+    .rocket-clouds__bubble, .rocket-clouds__cloud, .rocket-rocket, .rocket-inner__rocket-and-lines {
+      fill: #ffffff; }
+
+    </style>
+
+</head>
+
+<body class="gray-bg">
+
+
+    <div class="middle-box text-center animated fadeInDown">
+        <div class="text-center">
+
+            <svg id="Layer_1" class="svg-computer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 424.2 424.2">
+                <style>
+                    .st0{fill:none;stroke:#4489e4;stroke-width:5;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
+                </style>
+                <g id="Layer_2">
+                    <path class="st0" d="M339.7 289h-323c-2.8 0-5-2.2-5-5V55.5c0-2.8 2.2-5 5-5h323c2.8 0 5 2.2 5 5V284c0 2.7-2.2 5-5 5z"/>
+                    <path class="st0" d="M26.1 64.9h304.6v189.6H26.1zM137.9 288.5l-3.2 33.5h92.6l-4.4-33M56.1 332.6h244.5l24.3 41.1H34.5zM340.7 373.7s-.6-29.8 35.9-30.2c36.5-.4 35.9 30.2 35.9 30.2h-71.8z"/>
+                    <path class="st0" d="M114.2 82.8v153.3h147V82.8zM261.2 91.1h-147"/>
+                    <path class="st0" d="M124.5 105.7h61.8v38.7h-61.8zM196.6 170.2H249v51.7h-52.4zM196.6 105.7H249M196.6 118.6H249M196.6 131.5H249M196.6 144.4H249M124.5 157.3H249M124.5 170.2h62.2M124.5 183.2h62.2M124.5 196.1h62.2M124.5 209h62.2M124.5 221.9h62.2"/>
+                </g>
+            </svg>
+
+            <h4 class="text-custom">Site is Under Maintenance</h4>
+            <p class="text-muted">We're making the system more awesome.we'll be back shortly. :)</p>
+        </div>
+
+    </div>
+
+    <!-- Mainly scripts -->
+    <script type="text/javascript" src="{{ asset('assets/plugins/jquery-1.12.3.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/vendors/bootstrap/js/bootstrap.min.js') }}"></script>
+
+</body>
+
+</html>
