f480d61950d0c0f036d4f09994a25a6cf1305994
Iqbal Elfaita <iqbalelfaita@gmail.com>
2019-01-08 17:07:39 +0700

    iqbal

--- resources/views/manajemen_penjualan/analisis_penjualan/index.blade.php
+++ resources/views/manajemen_penjualan/analisis_penjualan/index.blade.php
@@ -146,50 +146,47 @@
 
                 /* pie chart */
 
-                if ($('#pie-chart').length) {
-
-                    var data_pie = [];
-                    for (var i = 0; i < response.data.data.length; i++) {
-                        data_pie[i] = {
-                            label : response.data.data[i].cat,
-                            data : response.data.data[i].qty
-                        }
+                var data_pie = [];
+                var series = response.data.data.length;
+                for (var i = 0; i < series; i++) {
+                    data_pie[i] = {
+                        label : response.data.data[i].cat,
+                        data : response.data.data[i].qty
                     }
-
-                    $.plot($("#pie-chart"), data_pie, {
-                        series : {
-                            pie : {
-                                show : true,
-                                innerRadius : 0.5,
-                                radius : 1,
-                                label : {
-                                    show : false,
-                                    radius : 2 / 3,
-                                    formatter : function(label, series) {
-                                        return '<div style="font-size:11px;text-align:center;padding:4px;color:white;">' + label + '<br/>' + Math.round(series.percent) + '%</div>';
-                                    },
-                                    threshold : 0.1
-                                }
-                            }
-                        },
-                        legend : {
-                            show : true,
-                            noColumns : 1, // number of colums in legend table
-                            labelFormatter : null, // fn: string -> string
-                            labelBoxBorderColor : "#000", // border color for the little label boxes
-                            container : null, // container (as jQuery object) to put legend in, null means default on top of graph
-                            position : "ne", // position of default legend container within plot
-                            margin : [5, 10], // distance from grid edge to default legend container within plot
-                            backgroundColor : "#efefef", // null means auto-detect
-                            backgroundOpacity : 1 // set to 0 to avoid background
-                        },
-                        grid : {
-                            hoverable : true,
-                            clickable : true
-                        },
-                    });
-
                 }
+
+                $.plot($("#pie-chart"), data_pie, {
+                    series : {
+                        pie : {
+                            show : true,
+                            innerRadius : 0.5,
+                            radius : 1,
+                            label : {
+                                show : false,
+                                radius : 2 / 3,
+                                formatter : function(label, series) {
+                                    return '<div style="font-size:11px;text-align:center;padding:4px;color:white;">' + label + '<br/>' + Math.round(series.percent) + '%</div>';
+                                },
+                                threshold : 0.1
+                            }
+                        }
+                    },
+                    legend : {
+                        show : true,
+                        noColumns : 1, // number of colums in legend table
+                        labelFormatter : null, // fn: string -> string
+                        labelBoxBorderColor : "#000", // border color for the little label boxes
+                        container : null, // container (as jQuery object) to put legend in, null means default on top of graph
+                        position : "ne", // position of default legend container within plot
+                        margin : [5, 10], // distance from grid edge to default legend container within plot
+                        backgroundColor : "#efefef", // null means auto-detect
+                        backgroundOpacity : 1 // set to 0 to avoid background
+                    },
+                    grid : {
+                        hoverable : true,
+                        clickable : true
+                    },
+                });
 
                 /* end pie chart */
             })
