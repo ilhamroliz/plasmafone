565f52b5014454567f9d515b06a7c2c3378caf11
syahrulyusuf97 <syahrulyusuf97@gmail.com>
2019-01-22 10:13:26 +0700

    plasmafone

--- resources/views/penjualan/penjualan-regular/index.blade.php
+++ resources/views/penjualan/penjualan-regular/index.blade.php
@@ -1229,47 +1229,7 @@
 				})
 		}
 
-		function formatRupiah2(angka, prefix = undefined)
-		{
-			var number_string = angka.toString(),
-				split	= number_string.split(','),
-				sisa 	= split[0].length % 3,
-				rupiah 	= split[0].substr(0, sisa),
-				ribuan 	= split[0].substr(sisa).match(/\d{1,3}/gi);
-				
-			if (ribuan) {
-				separator = sisa ? '.' : '';
-				rupiah += separator + ribuan.join('.');
-			}
-			
-			rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-			return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
-		}
 
-		function formatRupiah(angka, prefix = undefined)
-		{
-			var number_string = angka.replace(/[^,\d]/g, '').toString(),
-				split	= number_string.split(','),
-				sisa 	= split[0].length % 3,
-				rupiah 	= split[0].substr(0, sisa),
-				ribuan 	= split[0].substr(sisa).match(/\d{1,3}/gi);
-				
-			if (ribuan) {
-				separator = sisa ? '.' : '';
-				rupiah += separator + ribuan.join('.');
-			}
-			
-			rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-			return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
-		}
-
-		function isNumberKey(evt)
-		{
-		    var charCode = (evt.which) ? evt.which : evt.keyCode;
-		    if (charCode > 31 && (charCode < 48 || charCode > 57))
-		        return false;
-		    return true;
-		}
 
 		function validate(evt) {
 			var theEvent = evt || window.event;
