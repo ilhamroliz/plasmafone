4e19970e84276cb124cff7fb6d8eb13a6c227ba2
bayuwahono <bayuwahono33@gmail.com>
2019-01-24 17:45:08 +0700

    bayuwahono

--- app/Http/Controllers/keuangan/keuangan.php
+++ app/Http/Controllers/keuangan/keuangan.php
@@ -39,22 +39,146 @@ class keuangan extends Controller
     // view  data
     public function viewCoa_jenis()
     {
+        $jenis = DB::table('dk_akun_jenis')
+        ->select('dk_akun_jenis.*')
+        ->get();
 
+        $data = array();
+        foreach ($jenis as $key) {
+            $row = array();
+            $row[] = $key->dk_jenis_id;
+            $row[] = $key->dk_nama_jenis;
+            $data[] = $row;
+
+        }
+        echo json_encode(array("data"=>$data));
     }
 
    
-    public function viewCoa_kelompok()
+    public function viewCoa_kelompok(Request $request)
     {
+        $jenis = $request->input('jenis');
+        $kelompok = DB::table('dk_akun_kelompok')
+        ->select('dk_akun_kelompok.*','dk_akun_jenis.*')
+        ->join('dk_akun_jenis','dk_akun_kelompok.ACCJENIS','=','dk_akun_jenis.dk_jenis_id')
+        ->where('dk_akun_kelompok.ACCJENIS','=',$jenis)
+        ->get();
+
+        $data = array();
+        foreach ($kelompok as $key ) {
+           $row = array();
+           $row[] = $key->dk_nama_jenis;
+           $row[] = $key->ACCKEL;
+           $row[] = $key->KELOMPOK;
+           $data[] = $row;
+        }
+        echo json_encode(array("data"=>$data));
+    }
+
+    public function viewCoa_buku_besar(Request $request)
+    {
+        $jenis = $request->input('jenis');
+        $kelompok = $request->input('kelompok');
+
+        $bb = DB::table('dk_akun_bb')
+        ->select('dk_akun_bb.*','dk_akun_kelompok')
+        ->join('dk_akun_kelompok','dk_akun_bb.ACCKEL','=','dk_akun_kelompok.ACCKEL')
+        ->where('dk_akun_bb.ACCKEL','=',$kelompok)
+        ->get();
+
+        $data = array();
+        foreach ($bb as $key ) {
+            $row = array();
+            $row[] = $key->KELOMPOK;
+            $row[] = $key->ACCBB;
+            $row[] = $key->BUKUBESAR;
+            $data[] = $row;
+         }
+         echo json_encode(array("data"=>$data));
 
     }
 
-    public function viewCoa_buku_besar()
+    public function viewCoa_sub_buku_besar(Request $request)
     {
+        $jenis = $request->input('jenis');
+        $kelompok = $request->input('kelompok');
+        $buku_besar = $request->input('buku_besar');
 
-    }
+        if($jenis =='00' and $kelompok=='00' and $buku_besar=='00'){
+            $sbb = DB::table('dk_akun_sbb')
+            ->select('dk_akun_sbb.*','dk_akun_bb.*','dk_akun_kel.*','dk_akun_jenis.*')
+            ->join('dk_akun_bb','dk_akun_sbb.ACCBB','=','dk_akun_bb.ACCBB')
+            ->join('dk_akun_kel','dk_akun_bb.ACCKEL','=','dk_akun_kel.ACCKEL')
+            ->join('dk_akun_jenis','dk_akun_kel.ACCJENIS','=','dk_akun_jenis.ACCJENIS')
+            ->get();
+        }else if($jenis !='00' and $kelompok=='00' and $buku_besar=='00'){
+            $sbb = DB::table('dk_akun_sbb')
+            ->select('dk_akun_sbb.*','dk_akun_bb.*','dk_akun_kel.*','dk_akun_jenis.*')
+            ->join('dk_akun_bb','dk_akun_sbb.ACCBB','=','dk_akun_bb.ACCBB')
+            ->join('dk_akun_kel','dk_akun_bb.ACCKEL','=','dk_akun_kel.ACCKEL')
+            ->join('dk_akun_jenis','dk_akun_kel.ACCJENIS','=','dk_akun_jenis.ACCJENIS')
+            ->where('dk_akun_kel.ACCJENIS','=',$jenis)
+            ->get();
+        }else if($jenis =='00' and $kelompok !='00' and $buku_besar=='00'){
+            $sbb = DB::table('dk_akun_sbb')
+            ->select('dk_akun_sbb.*','dk_akun_bb.*','dk_akun_kel.*','dk_akun_jenis.*')
+            ->join('dk_akun_bb','dk_akun_sbb.ACCBB','=','dk_akun_bb.ACCBB')
+            ->join('dk_akun_kel','dk_akun_bb.ACCKEL','=','dk_akun_kel.ACCKEL')
+            ->join('dk_akun_jenis','dk_akun_kel.ACCJENIS','=','dk_akun_jenis.ACCJENIS')
+            ->where('dk_akun_bb.ACCKEL','=',$kelompok)
+            ->get();
+        }else if($jenis =='00' and $kelompok =='00' and $buku_besar !='00'){
+            $sbb = DB::table('dk_akun_sbb')
+            ->select('dk_akun_sbb.*','dk_akun_bb.*','dk_akun_kel.*','dk_akun_jenis.*')
+            ->join('dk_akun_bb','dk_akun_sbb.ACCBB','=','dk_akun_bb.ACCBB')
+            ->join('dk_akun_kel','dk_akun_bb.ACCKEL','=','dk_akun_kel.ACCKEL')
+            ->join('dk_akun_jenis','dk_akun_kel.ACCJENIS','=','dk_akun_jenis.ACCJENIS')
+            ->where('dk_akun_sbb.ACCBB','=',$buku_besar)
+            ->get();
+        }else if($jenis =='00' and $kelompok !='00' and $buku_besar !='00'){
+            $sbb = DB::table('dk_akun_sbb')
+            ->select('dk_akun_sbb.*','dk_akun_bb.*','dk_akun_kel.*','dk_akun_jenis.*')
+            ->join('dk_akun_bb','dk_akun_sbb.ACCBB','=','dk_akun_bb.ACCBB')
+            ->join('dk_akun_kel','dk_akun_bb.ACCKEL','=','dk_akun_kel.ACCKEL')
+            ->join('dk_akun_jenis','dk_akun_kel.ACCJENIS','=','dk_akun_jenis.ACCJENIS')
+            ->where('dk_akun_bb.ACCKEL','=',$kelompok)
+            ->where('dk_akun_sbb.ACCBB','=',$buku_besar)
+            ->get();
+        }else if($jenis !='00' and $kelompok !='00' and $buku_besar =='00'){
+            $sbb = DB::table('dk_akun_sbb')
+            ->select('dk_akun_sbb.*','dk_akun_bb.*','dk_akun_kel.*','dk_akun_jenis.*')
+            ->join('dk_akun_bb','dk_akun_sbb.ACCBB','=','dk_akun_bb.ACCBB')
+            ->join('dk_akun_kel','dk_akun_bb.ACCKEL','=','dk_akun_kel.ACCKEL')
+            ->join('dk_akun_jenis','dk_akun_kel.ACCJENIS','=','dk_akun_jenis.ACCJENIS')
+            ->where('dk_akun_kel.ACCJENIS','=',$jenis)
+            ->where('dk_akun_bb.ACCKEL','=',$kelompok)
+            ->get();
+        }else if($jenis !='00' and $kelompok !='00' and $buku_besar !='00'){
+            $sbb = DB::table('dk_akun_sbb')
+            ->select('dk_akun_sbb.*','dk_akun_bb.*','dk_akun_kel.*','dk_akun_jenis.*')
+            ->join('dk_akun_bb','dk_akun_sbb.ACCBB','=','dk_akun_bb.ACCBB')
+            ->join('dk_akun_kel','dk_akun_bb.ACCKEL','=','dk_akun_kel.ACCKEL')
+            ->join('dk_akun_jenis','dk_akun_kel.ACCJENIS','=','dk_akun_jenis.ACCJENIS')
+            ->where('dk_akun_kel.ACCJENIS','=',$jenis)
+            ->where('dk_akun_bb.ACCKEL','=',$kelompok)
+            ->where('dk_akun_sbb.ACCBB','=',$buku_besar)
+            ->get();
+        }
 
-    public function viewCoa_sub_buku_besar()
-    {
+        $data = array();
+        $i = 1;
+        foreach ($sbb as $key) {
+            $row = array();
+            $row[] = $i++;
+            $row[] = $key->dk_nama_jenis;
+            $row[] = $key->KELOMPOK;
+            $row[] = $key->BUKUBESAR;
+            $row[] = $key->ACC;
+            $row[] = $key->KETERANGAN;
+            $data[] = $row;
+        }
+
+        echo json_encode(array("data"=>$data));
 
     }
 
@@ -62,12 +186,78 @@ class keuangan extends Controller
 
     public function get_coaJenis()
     {
+        $jenis = DB::table('dk_akun_jenis')
+        ->select('dk_akun_jenis.*')
+        ->orderBy('ACCJENIS','ASC')
+        ->get();
+
+        echo json_encode($jenis);
 
     }
 
-    public function get_coaKelompok()
+    public function get_coaKelompok(Request $request)
     {
+        $jenis = $request->input('jenis');
 
+        if($jenis=='00'){
+            $kelompok = DB::table('dk_akun_kel')
+            ->select('dk_akun_kel.*')
+            ->orderBy('ACCKEL','ASC')
+            ->get();
+        }else{
+            $kelompok = DB::table('dk_akun_kel')
+            ->select('dk_akun_kel.*')
+            ->where('dk_akun_kel.ACCJENIS','=',$jenis)
+            ->orderBy('ACCKEL','ASC')
+            ->get();
+        }
+        
+
+        echo json_encode($kelompok);
+
+    }
+
+    public function get_coaBb(Request $request)
+    {
+        $jenis = $request->input('jenis');
+        $kelompok = $request->input('kelompok');
+
+        if($kelompok =='00'){
+            $buku_besar = DB::table('dk_akun_bb')
+            ->select('dk_akun_bb.*')
+            ->orderBy('ACCBB','ASC')
+            ->get();
+        }else{
+            $buku_besar = DB::table('dk_akun_bb')
+            ->select('dk_akun_bb.*')
+            ->where('dk_akun_bb.ACCKEL','=',$kelompok)
+            ->orderBy('ACCBB','ASC')
+            ->get();
+        }
+        
+
+        echo json_encode($buku_besar);
+
+    }
+
+    public function get_detail_bb(Request $request)
+    {
+        $bb = $request->input('bb');
+        $query = DB::table('dk_akun_bb')
+        ->select('dk_akun_bb.*')
+        ->where('dk_akun_bb.ACCBB','=',$bb)
+        ->get();
+
+        foreach ($query as $key) {
+            $ACCBB = $key->ACCBB;
+            $KATEGORI = $key->KATEGORI;
+        }
+        $data = array(
+            "accbb"=>$ACCBB,
+            "kategori"=>$KATEGORI
+        );
+
+        echo json_encode($data);
     }
 
     // tambah data
@@ -82,8 +272,34 @@ class keuangan extends Controller
 
     }
 
-    public function add_sub_buku_besar()
+    public function add_sub_buku_besar(Request $request)
     {
+        $accbb = $request->input('accbb');
+        $accsbb = $request->input('accsbb');
+        $keterangan = $request->input('keterangan');
+        $ket = strtoupper($keterangan);
+        $cashflow = $request->input('cashflow');
+        $unit = Auth::user()->m_comp;
+        $id_sbb = $accbb.''.$accsbb;
+
+        $list = array([
+            'ACCBB'     =>$accbb,
+            'ACC'       =>$id_sbb,
+            'KETERANGAN'=>$ket,
+            'CASHFLOW'  =>$cashflow,
+            'UNIT'      =>$unit
+            ]);
+
+        $insert = DB::table('dk_akun_sbb')->insert($list);
+        if(!$insert){
+            $data = "GAGAL";
+            echo json_encode(array("status"=>$data));
+        }else{
+            $data = "SUKSES";
+            echo json_encode(array("status"=>$data));
+        }
+
+
 
     }
 
