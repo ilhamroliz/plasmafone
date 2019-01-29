f868a2fa97199520dcb2c32b4e94e72440c1522e
masumam06 <aku.masumam@gmail.com>
2019-01-29 11:32:37 +0700

    umam

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -4,7 +4,7 @@
 <title> Plasmafone | @yield('title') </title>
 <meta name="description" content="">
 <meta name="author" content="">
-	
+
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 <!-- Basic Styles -->
@@ -37,7 +37,7 @@
 
 <!-- We recommend you use "your_style.css" to override SmartAdmin
      specific styles this will also ensure you retrain your customization with each SmartAdmin update.-->
-<link rel="stylesheet" type="text/css" media="screen" href="{{ asset('template_asset/css/your_style.css') }}"> 
+<link rel="stylesheet" type="text/css" media="screen" href="{{ asset('template_asset/css/your_style.css') }}">
 
 <!-- FAVICONS -->
 <link rel="shortcut icon" href="{{ asset('template_asset/img/favicon/logo_small.png') }}" type="image/x-icon">
@@ -46,7 +46,7 @@
 <!-- GOOGLE FONT -->
 <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
 
-<!-- Specifying a Webpage Icon for Web Clip 
+<!-- Specifying a Webpage Icon for Web Clip
 	 Ref: https://developer.apple.com/library/ios/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html -->
 <link rel="apple-touch-icon" href="{{ asset('template_asset/img/splash/sptouch-icon-iphone.png') }}">
 <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('template_asset/img/splash/touch-icon-ipad.png') }}">
@@ -61,6 +61,8 @@
 <link rel="apple-touch-startup-image" href="{{ asset('template_asset/img/splash/ipad-landscape.png') }}" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)">
 <link rel="apple-touch-startup-image" href="{{ asset('template_asset/img/splash/ipad-portrait.png') }}" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)">
 <link rel="apple-touch-startup-image" href="{{ asset('template_asset/img/splash/iphone.png') }}" media="screen and (max-device-width: 320px)">
+<!------------------------------------------------------------------------------------------------------------------------------------------->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 
 <style type="text/css">
 	.dataTables_length {
