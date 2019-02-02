8b116a21df869e5ddf49a616d0ec25fa7ce05139
ilhamroliz <ilham.rolis@gmail.com>
2019-02-02 13:01:08 +0700

    roliz

--- routes/web.php
+++ routes/web.php
@@ -448,29 +448,17 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/pembelian/purchase-order/list_draftPo', 'PembelianController@list_draftPo');
     Route::get('/pembelian/purchase-order/simpanPo', 'PembelianController@simpanPo');
 	// end aksi purchase
-
     Route::get('/pembelian/purchase-order/add', 'PembelianController@purchase_order_add');
-
     Route::get('/pembelian/purchase-order/get-purchase/{id}', 'PembelianController@get_purchase');
-
     Route::get('/pembelian/purchase-order/get-request-purchase/{id}', 'PembelianController@get_request_purchase');
-
     Route::post('/pembelian/purchase-order/add-purchase', 'PembelianController@add_purchase');
-
     Route::get('/pembelian/purchase-order/get/{id}', 'PembelianController@get_purchase_order');
-
     Route::get('/pembelian/purchase-order/edit', 'PembelianController@edit_purchase_order');
-
     Route::post('/pembelian/purchase-order/update', 'PembelianController@update_purchase_order');
-
     Route::post('/pembelian/purchase-order/edit-multiple', 'PembelianController@multiple_edit_purchase_order');
-
     Route::match(['get', 'post'], '/pembelian/purchase-order/multiple-delete', 'PembelianController@multiple_delete_purchase_order');
-
     Route::get('/pembelian/purchase-order/cetak', 'PembelianController@cetak_purchase');
-
     Route::get('/pembelian/purchase-order/get-purchase-data/{id}', 'PembelianController@get_purchase_data');
-
     Route::get('/pembelian/purchase-order/print/{id}', 'PembelianController@print_purchase');
     Route::get('/pembelian/purchase-order/purchase-pdf/{id}', 'PembelianController@viewpdf_purchase');
     Route::get('/pembelian/purchase-order/generate-pdf/{id}', 'PembelianController@pdf_purchase');
