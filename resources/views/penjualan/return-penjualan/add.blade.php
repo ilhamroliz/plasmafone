f3549e7394952dcdffc8036ba017e816350648de
syahrulyusuf97 <syahrulyusuf97@gmail.com>
2019-02-06 10:19:50 +0700

    plasmafone

--- resources/views/penjualan/return-penjualan/add.blade.php
+++ resources/views/penjualan/return-penjualan/add.blade.php
@@ -58,7 +58,7 @@
                          data-widget-custombutton="false"
                          data-widget-sortable="false">
                         <header role="heading"><div class="jarviswidget-ctrls" role="menu"><a href="javascript:void(0);" class="button-icon jarviswidget-toggle-btn" rel="tooltip" title="" data-placement="bottom" data-original-title="Collapse"><i class="fa fa-minus "></i></a> <a href="javascript:void(0);" class="button-icon jarviswidget-fullscreen-btn" rel="tooltip" title="" data-placement="bottom" data-original-title="Fullscreen"><i class="fa fa-expand "></i></a> </div>
-                            <h2><strong>Tambah Layanan Perbaikan</strong></h2>
+                            <h2><strong>Tambah Retun Penjualan</strong></h2>
                         </header>
                         <div role="content">
                             <div class="widget-body">
@@ -74,6 +74,7 @@
                                                             <div class="icon-addon addon-md">
                                                                 <input class="form-control" id="cari-member" placeholder="Masukkan Nama Member" type="text"  style="text-transform: uppercase">
                                                                 <label for="cari-member" class="glyphicon glyphicon-search" rel="tooltip" title="Nama Member"></label>
+                                                                <input type="hidden" name="idmember" id="idmember">
                                                             </div>
                                                             <span class="input-group-btn">
 										                        <button class="btn btn-primary" type="button" id="search_member"><i class="fa fa-search"></i> Cari</button>
@@ -89,7 +90,6 @@
                                                             <span class="input-group-addon"><i class="glyphicon glyphicon-barcode"></i></span>
                                                             <div class="icon-addon addon-md">
                                                                 <input class="form-control" id="kode" placeholder="Masukkan Kode Spesifik" type="text"  style="text-transform: uppercase">
-                                                                <input type="hidden" name="code" id="code">
                                                                 <label for="kode" class="glyphicon glyphicon-search" rel="tooltip" title="Kode Spesifik"></label>
                                                             </div>
                                                             <span class="input-group-btn">
@@ -105,9 +105,23 @@
                                                         <div class="input-group input-group-md">
                                                             <span class="input-group-addon"><i class="glyphicon glyphicon-book"></i></span>
                                                             <div class="icon-addon addon-md">
-                                                                <input class="form-control" id="nota" placeholder="Masukkan Nota Pembelian" type="text"  style="text-transform: uppercase">
-                                                                <label for="nota" class="glyphicon glyphicon-search" rel="tooltip" title="Nota Pembelian"></label>
+                                                                <input class="form-control" id="nota" placeholder="Masukkan Nota Penjualan" type="text"  style="text-transform: uppercase">
+                                                                <label for="nota" class="glyphicon glyphicon-search" rel="tooltip" title="Nota Penjualan"></label>
                                                             </div>
+                                                            <span class="input-group-btn">
+										                        <button class="btn btn-primary" type="button" id="search_nota"><i class="fa fa-search"></i> Cari</button>
+										                    </span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label class="col-md-3 control-label text-left">Tanggal</label>
+                                                    <div class="col-md-9">
+                                                        <div class="input-group input-daterange" id="date-range">
+                                                            <input type="text" class="form-control" id="tgl_awal" name="tgl_awal"  placeholder="Tanggal Awal" data-dateformat="dd/mm/yy">
+                                                            <span class="input-group-addon bg-custom text-white b-0">to</span>
+                                                            <input type="text" class="form-control" id="tgl_akhir" name="tgl_akhir"  placeholder="Tanggal Akhir" data-dateformat="dd/mm/yy">
                                                             <span class="input-group-btn">
 										                        <button class="btn btn-primary" type="button" id="search_nota"><i class="fa fa-search"></i> Cari</button>
 										                    </span>
@@ -125,10 +139,71 @@
             </div>
             <!-- end row -->
         </section>
+
+        <section id="widget-grid" class="">
+
+            <!-- row -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="jarviswidget" id="wid-id-0"
+                         data-widget-colorbutton="false"
+                         data-widget-editbutton="false"
+                         data-widget-togglebutton="false"
+                         data-widget-deletebutton="false"
+                         data-widget-fullscreenbutton="false"
+                         data-widget-custombutton="false"
+                         data-widget-sortable="false">
+                        <header role="heading"><div class="jarviswidget-ctrls" role="menu"><a href="javascript:void(0);" class="button-icon jarviswidget-toggle-btn" rel="tooltip" title="" data-placement="bottom" data-original-title="Collapse"><i class="fa fa-minus "></i></a> <a href="javascript:void(0);" class="button-icon jarviswidget-fullscreen-btn" rel="tooltip" title="" data-placement="bottom" data-original-title="Fullscreen"><i class="fa fa-expand "></i></a> </div>
+                            <h2><strong>Daftar Nota Penjualan</strong></h2>
+                        </header>
+                        <div role="content">
+                            <div class="widget-body no-padding">
+                                <table id="dt_active" class="table table-striped table-bordered table-hover" width="100%">
+
+                                    <thead>
+
+                                    <tr>
+
+                                        <th><i class="fa fa-fw fa-calendar txt-color-blue"></i>&nbsp;Tanggal</th>
+
+                                        <th><i class="fa fa-fw fa-building txt-color-blue "></i>&nbsp;Nota</th>
+
+                                        <th class="text-center" width="15%"><i class="fa fa-fw fa-wrench txt-color-blue"></i>&nbsp;Aksi</th>
+
+                                    </tr>
+
+                                    </thead>
+
+                                    <tbody>
+
+                                    </tbody>
+
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end row -->
+        </section>
     </div>
     <!-- END MAIN CONTENT -->
 @endsection
 
 @section('extra_script')
+    <script type="text/javascript">
+        var aktif;
+        $(document).ready(function () {
+            aktif = $('#dt_active').dataTable();
 
+            $( "#date-range" ).datepicker({
+                language: "id",
+                format: 'dd/mm/yyyy',
+                prevText: '<i class="fa fa-chevron-left"></i>',
+                nextText: '<i class="fa fa-chevron-right"></i>',
+                autoclose: true,
+                todayHighlight: true
+            });
+        })
+    </script>
 @endsection
